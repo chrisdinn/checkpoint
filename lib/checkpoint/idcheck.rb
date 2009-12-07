12f7bc6db017eb2837659119c436947b9c4ba212
Chris Dinn <chris@massaudience.ca>
2009-12-07 12:38:42 -0500

    Some key changes.

--- lib/checkpoint/idcheck.rb
+++ lib/checkpoint/idcheck.rb
@@ -7,13 +7,16 @@ module Checkpoint
     end
     
     def call(env)
-      if (env['rack.session'] && env['rack.session'][:checkpoint_user_id]) || env["PATH_INFO"]=~Regexp.new('/sso')
+      if session_exists?(env) || env["PATH_INFO"]=~Regexp.new('/sso')
         @app.call(env)
       else
         return [301, {'Location' => '/sso/login', 'Content-Type'=>'text/html'}, "Redirecting to sign in page"] 
       end
     end
     
+    def session_exists?(env)
+      env.has_key?('rack.session') && env['rack.session'].has_key?(:checkpoint_user_id)
+    end
   end
   
 end
